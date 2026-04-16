Fix camera screen scaling

camera.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Camera:
 
-    def __init__(self, pos, screen_dist=25, screen_pos=(0, 0), screen_size=2):
+    def __init__(self, pos, screen_dist=1, screen_pos=(0, 0), screen_size=1):
         self.pos = np.array(pos)
         self.screen_dist = screen_dist   # The distance of the screen
         self.screen_pos = screen_pos    # The pos of the screen relative to the camera
@@ -14,8 +14,8 @@ class Camera:
     def make_screen(self, ratio):
         a = -self.screen_size - self.screen_pos[0]
         b = self.screen_size/ratio + self.screen_pos[1]
-        c = self.screen_size - (self.screen_size - self.screen_pos[0])
-        d = -self.screen_size/ratio + (self.screen_size/ratio - self.screen_pos[1])
+        c = self.screen_size - self.screen_pos[0]
+        d = -self.screen_size/ratio + self.screen_pos[1]
         self.screen = (a, b, c, d)
 
 

renderer.py
@@ -65,15 +65,12 @@ def render(viewport: Viewport):
     # For each pixel of the image:
     for i, y in enumerate(v):
         for j, x in enumerate(h):
-            pixel = np.array([x, y, 0])  # D (for destination) | z = 0 since the camera is on the x, y plane | Might add tilted camera support later
+            pixel = np.array([x, y, 0])   # D (for destination) | z = 0 since the camera is on the x, y plane | Might add tilted camera support later
             origin = viewport.camera.pos  # O (for origin
             direction = normalize(pixel - origin)  # d = D - O / || D - O || (for direction)
 
             dist = nearest_intersect_object(viewport.objects, origin, direction)[1] / view_dist
             set_color(i, j, image, 1 - min(dist, 1))
 
-        print(f"progress: {(i + 1) * 100 // viewport.height}%")
+        # print(f"progress: {(i + 1) * 100 // viewport.height}%")
     return image
-
-
-plt.imsave(f'img/image.png', render(viewport))